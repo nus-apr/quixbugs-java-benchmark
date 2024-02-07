b75ba28a4226962a43743615d6bd20c70f50e307
Martin Mirchev <mmirchev@comp.nus.edu.sg>
2024-02-07 16:00:40 +0800

    Update paths

--- format_updater.py
+++ format_updater.py
@@ -7,7 +7,7 @@ x.close()
 for entry in contents:
     entry["language"] = "java"
     entry["src"] = {
-        "root_abspath": "/experiment/{subject}/{bug_id}/src".format(
+        "root_abspath": "/experiment/quixbugsjava/{subject}/{bug_id}/src".format(
             subject=entry["subject"], bug_id=entry["bug_id"]
         ),
         "entrypoint": {
